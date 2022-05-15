cfd41859f97a5a1fbb6a59e0728e12ff0abb5186
klubni1chka <97652254+klubni1chka@users.noreply.github.com>
2022-05-16 02:00:49 +0600

    aaa

---
+++ config/database.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //url: 'mongodb://localhost:27017/wednesday'
+    url:'mongodb+srv://Assel:18AhYuNjzP4kpnVS@cluster0.sefax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
